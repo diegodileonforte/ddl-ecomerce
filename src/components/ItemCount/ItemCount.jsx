fd8b2baed02004fbbfa1bdd4a052e046045923b8
Diego Di Leonforte <dileonfortediego@gmail.com>
2021-02-01 12:32:39 -0300

    Ajustes a ItemCount component

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -6,6 +6,19 @@ const ItemCountComponent = ({ initial, stock, onAdd }) => {
 
     const [count, setCount] = useState(initial)
 
+    const restar = () => {
+        if (count > initial) {
+            setCount(count - 1)
+        }
+
+    }
+
+    const sumar = () => {
+        if (count < stock) {
+            setCount(count + 1)
+        }
+    }
+
     if (initial === 0) {
 
         return <div className='container d-flex justify-content-center text-align-center' ><h1>Producto sin stock</h1></div>
@@ -16,23 +29,11 @@ const ItemCountComponent = ({ initial, stock, onAdd }) => {
         return (
             <div className='container'>
                 <div className='d-flex justify-content-around text-align-center' >
-                    <Button variant="outline-danger" size="sm" onClick={() => {
-
-                        if (count > initial) {
-                            setCount(count - 1)
-                        }
-
-                    }}>-</Button>
+                    <Button variant="outline-danger" size="sm" onClick={() => restar()}>-</Button>
 
                     <h1 className="text-align-center">{count}</h1>
 
-                    <Button variant="outline-primary" size='sm' onClick={() => {
-
-                        if (count < stock) {
-                            setCount(count + 1)
-                        }
-
-                    }}>+</Button>
+                    <Button variant="outline-primary" size='sm' onClick={() => sumar()}>+</Button>
 
                 </div>
                 <div className="d-flex justify-content-center">
